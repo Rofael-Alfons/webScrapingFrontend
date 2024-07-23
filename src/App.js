12681005057ce8931b4e360313267eaa6906c287
Rofael-Alfons <rofa.alfons7@gmail.com>
2024-07-24 01:23:50 +0300

    render url added

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function App() {
     setTypewriterDone(false);
     try {
       const response = await axios.post(
-        `http://localhost:5000/api/scrape/?url=${encodeURIComponent(url)}`
+        `https://equipment-602z.onrender.com/api/scrape/?url=${encodeURIComponent(
+          url
+        )}`
       );
       const summaryText = response.data.data;
       setSummary(summaryText);
